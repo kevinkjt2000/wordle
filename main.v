ce50a0b3ce44debcac7d3ac6a396283569b60386
Kevin Tindall <kevinkjt2000@gmail.com>
2022-06-14 19:26:51 -0500

    Print leftover dictionary words; add future TODOs

--- main.v
+++ main.v
@@ -17,7 +17,8 @@ struct WordleState {
 	dictionary []string
 	answer     string
 mut:
-	guesses []string
+	guesses           []string
+	remaining_answers []string
 }
 
 enum LetterColor {
@@ -38,8 +39,8 @@ fn print_guess(colors []LetterColor, guess string, show_letters bool) {
 
 fn new_wordle(wc WordleConfig) WordleState {
 	mut answer := wc.answer
+	answers := os.read_lines(wc.answers_file) or { [] }
 	if wc.answer == '' {
-		answers := os.read_lines(wc.answers_file) or { [] }
 		random_answer_index := rand.int_in_range(0, answers.len) or { 0 }
 		answer = answers[random_answer_index]
 	}
@@ -48,16 +49,12 @@ fn new_wordle(wc WordleConfig) WordleState {
 		config: wc
 		dictionary: dictionary
 		answer: answer
+		remaining_answers: answers
 	}
 	return ws
 }
 
-fn (mut ws WordleState) check_guess(guess string) []LetterColor {
-	assert ws.guesses.len < ws.config.max_guesses
-	assert guess.runes().len == ws.config.word_length
-	assert guess in ws.dictionary
-	ws.guesses << guess
-
+fn (ws WordleState) color_guess(guess string) []LetterColor {
 	mut letter_colors := []LetterColor{len: ws.config.word_length, init: LetterColor.gray}
 	for i, guess_rune in guess.runes() {
 		answer_rune := ws.answer.runes()[i]
@@ -79,19 +76,58 @@ fn (mut ws WordleState) check_guess(guess string) []LetterColor {
 			}
 		}
 	}
+	return letter_colors
+}
+
+fn (mut ws WordleState) submit_guess(guess string) {
+	assert ws.guesses.len < ws.config.max_guesses
+	assert guess.runes().len == ws.config.word_length
+	assert guess in ws.dictionary
+	ws.guesses << guess
+
+	letter_colors := ws.color_guess(guess)
 	print_guess(letter_colors, guess, ws.config.show_letters)
 	println('')
-	return letter_colors
+	ws.eliminate_invalid_answers()
+	ws.print_leftover_words()
+}
+
+fn (mut ws WordleState) eliminate_invalid_answers() {
+	mut answers := ws.remaining_answers
+	for guess in ws.guesses {
+		letter_colors := ws.color_guess(guess)
+		for i, color in letter_colors {
+			if color == .green {
+				answers = answers.filter(guess[i] == it[i])
+			} else if color == .yellow {
+				answers = answers.filter(guess[i] in it.bytes())
+			} else if color == .gray {
+				// TODO: obviously this is a buggy implementation that would fail if there were duplicates of a letter
+				answers = answers.filter(guess[i] !in it.bytes())
+			}
+		}
+	}
+	ws.remaining_answers = answers
+}
+
+fn (ws WordleState) print_leftover_words() {
+	println('Words left:')
+	println(ws.remaining_answers.len)
+	if ws.remaining_answers.len < 40 {
+		for word in ws.remaining_answers {
+			println(word)
+		}
+	}
 }
 
 fn main() {
 	show_letters := '--show-letters' in os.args[1..]
 	println("Hi, I'm gonna be a wordle solver!")
-	mut ws := new_wordle(show_letters: show_letters)
-	ws.check_guess('purse')
-	ws.check_guess('steak')
-	ws.check_guess('class')
-	ws.check_guess('floss')
-	ws.check_guess('fetch')
-	ws.check_guess('churn')
+	mut ws := new_wordle(show_letters: show_letters, answer: 'atone')
+	ws.submit_guess('steal')
+	ws.submit_guess('atone')
+
+	// TODO: looping over all possible wordles to apply different AIs
+	// TODO: scoring wordles after they are done to evaluate which AI is best
+	// TODO: not crashing on guess 7, i.e. stopping on guess 6 or before if it's solved early
 }
--- main_test.v
+++ main_test.v
@@ -1,16 +1,16 @@
 module main
 
-fn test_check_guess_produces_correct_colors() {
+fn test_submit_guess_produces_correct_colors() {
 	mut ws := new_wordle(answer: 'happy')
-	assert ws.check_guess('steak') == [.gray, .gray, .gray, .yellow, .gray]
-	assert ws.check_guess('takes') == [.gray, .green, .gray, .gray, .gray]
-	assert ws.check_guess('panda') == [.yellow, .green, .gray, .gray, .gray]
-	assert ws.check_guess('happy') == [.green, .green, .green, .green, .green]
+	assert ws.color_guess('steak') == [.gray, .gray, .gray, .yellow, .gray]
+	assert ws.color_guess('takes') == [.gray, .green, .gray, .gray, .gray]
+	assert ws.color_guess('panda') == [.yellow, .green, .gray, .gray, .gray]
+	assert ws.color_guess('happy') == [.green, .green, .green, .green, .green]
 
 	ws = new_wordle(answer: 'sweet')
-	assert ws.check_guess('murky') == [.gray, .gray, .gray, .gray, .gray]
-	assert ws.check_guess('takes') == [.yellow, .gray, .gray, .green, .yellow]
-	assert ws.check_guess('joint') == [.gray, .gray, .gray, .gray, .green]
-	assert ws.check_guess('fleet') == [.gray, .gray, .green, .green, .green]
-	assert ws.check_guess('sweet') == [.green, .green, .green, .green, .green]
+	assert ws.color_guess('murky') == [.gray, .gray, .gray, .gray, .gray]
+	assert ws.color_guess('takes') == [.yellow, .gray, .gray, .green, .yellow]
+	assert ws.color_guess('joint') == [.gray, .gray, .gray, .gray, .green]
+	assert ws.color_guess('fleet') == [.gray, .gray, .green, .green, .green]
+	assert ws.color_guess('sweet') == [.green, .green, .green, .green, .green]
 }
